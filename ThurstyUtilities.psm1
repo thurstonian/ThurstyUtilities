ca70de70735493d41be4e1ab75fb677e4cd435a4
Simon Thurston <44074551+thurstonian@users.noreply.github.com>
2024-10-03 15:38:06 -0400

    Version 0.1.1 (#7)
    * fixed exhibit stamp path
    
    * add function to restart onelog on local computer
    
    * add comments, remove unnecessary LAPS function
    
    * add LAPS Entra/Azure wrapper
    
    * adjust scopes to not use intune since we can grab device ids from entra
    
    * bump version to 0.1.1
    
    * update readme

--- ThurstyUtilities.psm1
+++ ThurstyUtilities.psm1
@@ -1,3 +1,5 @@
+# Copies exhibit stamps file from network share to stamps folder for user.
+# Works remotely
 function Add-ExhibitStamps {
 	[CmdletBinding()]
 	param(
@@ -6,14 +8,29 @@ function Add-ExhibitStamps {
 		[Parameter(Mandatory)]
 		[String]$UserName
 	)
-	Copy-Item -Path "\\cozen\deploy\source\Adobe\Pro DC\Exhibit Stamp\Exhibit-Stamp.pdf" -Destination "\\$ComputerName\c$\Users\$UserName\Adobe\Acrobat\DC\Stamps"
+	Copy-Item -Path "\\cozen\deploy\source\Adobe\Pro DC\Exhibit Stamp\Exhibit-Stamp.pdf" -Destination "\\$ComputerName\c$\Users\$UserName\AppData\Roaming\Adobe\Acrobat\DC\Stamps"
 }
 
+# Helper function to connect to Exchange Online Powershell using currently signed on user
 function Connect-EXO {
 	$Identity = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name -Split "\\"
 	Connect-ExchangeOnline -UserPrincipalName ("" + $Identity[1] + "@" + $Identity[0] + ".com") -ShowBanner:$false
 }
 
+# The new LAPS command is slow and sucks. Let's fix that.
+function Get-LAPSAzurePassword {
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory)]
+		[String]$ComputerName
+	)
+	Test-MgGraph
+	Connect-MgGraph -Scopes "Device.Read.All", "DeviceLocalCredential.Read.All" -NoWelcome
+	Get-LapsAADPassword -DeviceIds (Get-MgDevice -Filter "DisplayName eq '$ComputerName'").DeviceId -IncludePasswords -AsPlainText
+	(Disconnect-MgGraph) > nul
+}
+
+# Installs the latest version of Winget onto the current computer
 function Install-WinGet {
 	If ($null -ne (Get-Command "winget" -ErrorAction SilentlyContinue)) {
 		Write-Host "Winget is already installed."
@@ -31,6 +48,7 @@ function Install-WinGet {
 	Add-AppxPackage ($env:TEMP + "winget.msixbundle")
 }
 
+# Generates a new tap for a user account given an email address
 function New-TAP {
 	[CmdletBinding()]
 	param (
@@ -50,6 +68,7 @@ function New-TAP {
 	(Disconnect-MgGraph) >nul
 }
 
+# Removes the Bad iManage Adobe Reader addin from a remote computer
 function Remove-ReaderAddin {
 	[CmdletBinding()]
 	param(
@@ -113,6 +132,9 @@ function Remove-ReaderAddin {
 	}
 }
 
+# Removes Windows Hello pin from the current computer
+# Doesn't work right now despite testing
+# God help your soul if you try to fix it
 function Remove-WindowsHelloPin {
 	Test-ElevatedPrivileges
 	takeown /f "C:\Windows\ServiceProfiles\LocalService\AppData\Local\Microsoft\Ngc" /r /d y >nul
@@ -120,23 +142,31 @@ function Remove-WindowsHelloPin {
 	Remove-Item -Path "C:\WINDOWS\ServiceProfiles\LocalService\AppData\Local\Microsoft\Ngc" -Recurse -Force
 }
 
-function Set-LAPSPassword {
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory)]
-		[String]$ComputerName
-	)
-	$Password = Get-LapsADPassword $ComputerName -AsPlainText
-	Write-Host ("Password: " + $Password.Password)
-	Write-Host ("Expiration: " + $Password.ExpirationTimestamp)
+# Restarts Onelog on the current computer
+function Restart-OneLog {
+	$Path = "\ITS\OneLog\Client\LoginApplication.exe"
+	$Service = Get-Service -Name "ITS Onelog Client"
+	If ($Service.Status -eq "Running") {
+		Restart-Service -InputObject $Service -Force
+	} Else {
+		Start-Service -InputObject $Service
+	}
+	Get-Process -Name LoginApplication | Stop-Process
+	If (Test-Path -Path "$ENV:ProgramFiles$Path") {
+		& "$ENV:ProgramFiles$Path"
+	} ElseIf (Test-Path -Path "${ENV:ProgramFiles(x86)}$Path") {
+		& "${ENV:ProgramFiles(x86)}$Path"
+	}
 }
 
+# Stops all umbrella services on the current computer
 function Stop-Umbrella {
 	Test-ElevatedPrivileges
 	Get-Service -Name "*umbrellaagent*" | Where-Object { $_.Status -eq "Running" } | Stop-Service
 	Get-Service -Name "*swgagent*" | Where-Object { $_.Status -eq "Running" } | Stop-Service
 }
 
+# Checks AD for users who have Adobe Licenses
 function Test-AdobeLicense {
 	[CmdletBinding()]
 	param(
@@ -146,6 +176,7 @@ function Test-AdobeLicense {
     (Get-ADGroup -Identity "Adobe Pro Licensed Users" -Properties Member).Member | Select-String -Pattern $SearchFor
 }
 
+# Checks if the current session is running as Admin
 function Test-ElevatedPrivileges {
 	[CmdletBinding()]
 	$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
@@ -155,6 +186,7 @@ function Test-ElevatedPrivileges {
 	}
 }
 
+# Checks if Exchange Online Powershell is currently connected
 function Test-EXOConnection {
 	If ($null -ne (Get-ConnectionInformation)) {
 		Write-Error "Exchange Online already connected. Disconnecting..."
@@ -162,6 +194,7 @@ function Test-EXOConnection {
 	}
 }
 
+# Checks if an email address is on Exchange Online or not
 function Test-EXOMoved {
 	[CmdletBinding()]
 	param (
@@ -180,6 +213,7 @@ function Test-EXOMoved {
 	}
 }
 
+# Checks if Microsoft Graph is connected
 function Test-MgGraph {
 	If ($null -ne (Get-MgContext)) {
 		Write-Error "Microsoft Graph already connected. Disconnecting..."
